9a31e9416ac9c3d572d014b8e944003cd6c548d1
marcortiztorres <ortiztorresmarc@gmail.com>
2024-11-14 18:21:58 +0100

    :sparkles: First version of the About Me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { FrontPageComponent } from './components/front-page/front-page.component
 import { TripsSchedulledComponent } from './components/trips-schedulled/trips-schedulled.component';
 import { TripDescriptionComponent } from './components/trip-description/trip-description.component';
 import { TripInscriptionFormComponent } from './components/trip-inscription-form/trip-inscription-form.component';
+import { ContactComponent } from "./components/contact/contact.component";
 // Services
 import { provideHttpClient } from '@angular/common/http';
 // Extra
 import { MatGridList, MatGridTile } from "@angular/material/grid-list";
 import { AppRoutingModule } from './app-routing.module';
-import {MatIcon} from "@angular/material/icon";
+import { MatIcon } from "@angular/material/icon";
 
 
 @NgModule({
@@ -21,19 +22,21 @@ import {MatIcon} from "@angular/material/icon";
     RootComponent,
     TripDescriptionComponent
   ],
-    imports: [
-        BrowserModule,
-        MatGridList,
-        MatGridTile,
-        AppRoutingModule,
-        TripInscriptionFormComponent,
-        MatIcon
-    ],
+  imports: [
+    BrowserModule,
+    MatGridList,
+    MatGridTile,
+    AppRoutingModule,
+    TripInscriptionFormComponent,
+    MatIcon,
+    ContactComponent,
+  ],
   exports: [
     FrontPageComponent,
     TripsSchedulledComponent,
     RootComponent,
-    TripDescriptionComponent
+    TripDescriptionComponent,
+    ContactComponent
   ],
   providers: [provideHttpClient()],
   bootstrap: [RootComponent]
